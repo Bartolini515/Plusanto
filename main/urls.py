cec773676e13c1de56a84d4facccf940c68a6f1e
Bartolini515 <bartolini0690@gmail.com>
2024-11-19 22:43:42 +0100

    Zmiana nazwy podstrony informacje na budżet

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), # Przekierowanie na stronę główną w przypadku wejścia na stronę
     path('dashboard/', views.dashboard, name='dashboard'), # Przekierowanie na pulpit
-    path('dashboard/informations/', views.informations, name='informations'), # Przekierowanie na informacje użytkownika
-    path('about/', views.about, name='about'), # Przekierowanie na pulpit
-    path('contact/', views.contact, name='contact') # Przekierowanie na pulpit
+    path('dashboard/budget/', views.budget, name='budget'), # Przekierowanie na budżet
+    path('about/', views.about, name='about'), # Przekierowanie na stronę o nas
+    path('contact/', views.contact, name='contact') # Przekierowanie na stronę kontaktową
 ]
